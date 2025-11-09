10f90f3ba554daa9f31704882b875ac63361eb5e
BladeX <ivangabriel21210@gmail.com>
2025-11-09 11:14:36 -0400

    Update install-ws.sh

--- install-ws.sh
+++ install-ws.sh
@@ -2,5 +2,11 @@
 apt update -y
 apt install -y python2
 chmod +x websocket973.py
+
+# Usa el puerto que Cloud Run asigna
+PORT=${PORT:-8080}
+
 useradd -e 2025-12-31 Hola && echo "Hola:Mundo" | chpasswd
-python2 websocket973.py 8080
+
+echo "Iniciando WebSocket en puerto $PORT ..."
+python2 websocket973.py $PORT
